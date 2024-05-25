2dadb1c8099f54ea4a691cf7db081347cb392597
skm1803 <shubham.mishrakiit@gmail.com>
2024-05-25 10:19:39 +0530

    User SignIn SignUp implementation

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import Example from './components/LandingPage'
+// import SignIn from './components/SignIn'
+// import SignUp from './components/SignUp'
+import PropertyForm from './components/PropertyForm'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <Example/> */}
+      {/* <SignIn/> */}
+      {/* <SignUp/> */}
+      <PropertyForm/>
+
+    </>
+  )
+}
+
+export default App
