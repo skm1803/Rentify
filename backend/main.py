3fa124039973670441cd9989e7d3fbbdf72e3549
sourabh27 <sourabh@chat360.io>
2024-05-25 12:54:18 +0530

    support table creation and database connection

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-
+import schemas
 
 app = FastAPI()
-
 
 @app.get("/")
 def hello():
     return {"Hello": "World"}
+
+@app.post("/signup")
+def signup(user: schemas.UserSchema):
+    return user.dict()
+
---
+++ backend/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+from pydantic import validator
+
+
+class UserSchema(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: str
+    password: str
+
+    @validator('email')
+    def email_must_contain_gmail(cls, v):
+        if 'admin' in v:
+            raise ValueError('Admins is not an available keyword.')
+        return v
