70ac3c546b646a6e02957ebcc8e05b3772aec28d
sourabh27 <sourabh@chat360.io>
2024-05-26 09:25:57 +0530

    support for jwt authentication

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,4 +1,9 @@
+import os
+from datetime import datetime, timedelta
+
+from jose import jwt
 from passlib.context import CryptContext
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -11,3 +16,14 @@ class Hasher():
     @staticmethod
     def get_password_hash(password):
         return pwd_context.hash(password)
+    
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(days=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, os.getenv("JWT_SECRET_KEY"), algorithm=os.getenv("JWT_ALGORITHM"))
+    return encoded_jwt
